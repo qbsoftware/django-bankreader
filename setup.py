ba654d9d4720671fb646e9166ca8d9fefef52a03
Jakub Dorňák <jakub.dornak@misli.cz>
2018-10-05 17:44:52 +0200

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 
 setup(
     name='django-bankreader',
-    version='0.2.0',
+    version='0.2.1',
     description='Pluggable django application for reading and processing various formats of bank account statements',
     long_description=long_description,
     author='Jakub Dorňák',
